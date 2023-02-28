057be74a8218a67fd102c18f5cd67f777d1e6888
Wilson Junior <wilfer.wil@gmail.com>
2023-02-28 20:16:34 -0300

    Testing text-davinci-003 GPT-3

--- exploring-openai-chatgpt.py
+++ exploring-openai-chatgpt.py
@@ -8,18 +8,25 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 # Define a function to ask questions to ChatGPT
 def ask_chat_gpt(prompt):
     response = openai.Completion.create(
-        engine="text-davinci-002",
+        engine="text-davinci-003",
         prompt=prompt,
         max_tokens=1024,
         n=1,
         stop=None,
-        temperature=0.5,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0,
     )
 
     answer = response.choices[0].text.strip()
     return answer
 
-# Example usage
-answer = ask_chat_gpt(sys.argv[1])
+if len(sys.argv) > 1:
+    prompt = sys.argv[1]
+else:
+    prompt = input("Ask me anything: ")
+
+answer = ask_chat_gpt(prompt)
+
 print(answer)
 
