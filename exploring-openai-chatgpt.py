07d8b50739da247fa662e0da802704b340df97b3
Wilson Junior <wilfer.wil@gmail.com>
2023-02-28 19:43:07 -0300

    Setting text-davinci-002 as text completion engine

--- exploring-openai-chatgpt.py
+++ exploring-openai-chatgpt.py
@@ -1,5 +1,6 @@
 import openai
 import os
+import sys
 
 # Set up OpenAI API credentials
 openai.api_key = os.environ["OPENAI_API_KEY"]
@@ -7,19 +8,18 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 # Define a function to ask questions to ChatGPT
 def ask_chat_gpt(prompt):
     response = openai.Completion.create(
-        engine="davinci",
+        engine="text-davinci-002",
         prompt=prompt,
         max_tokens=1024,
         n=1,
         stop=None,
-        temperature=0.7,
+        temperature=0.5,
     )
 
     answer = response.choices[0].text.strip()
     return answer
 
 # Example usage
-prompt = "What is the meaning of life?"
-answer = ask_chat_gpt(prompt)
+answer = ask_chat_gpt(sys.argv[1])
 print(answer)
 
